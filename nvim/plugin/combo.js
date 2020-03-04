22d0ec2151ec352cf5a5897143cad7984b22c5b0
Danielo Rodriguez <rdanielo@gmail.com>
2020-03-04 10:01:12 +0100

    fix: combo enum name

--- nvim/plugin/combo.js
+++ nvim/plugin/combo.js
@@ -5,11 +5,12 @@ if (keys.length <= 0) {
   console.error("Not enough arguments");
   process.exit(1);
 }
-const combo_enum=`${keys.join('_')}`
+const combo_enum=`${keys.join('_').toUpperCase()}`
 const combo_name=`${keys.join('_')}_combo`
 const real_keys=keys.map(k => `KC_${k.toUpperCase()}`)
 const combo_definition = `const uint16_t PROGMEM ${combo_name}[]= {${real_keys.join(',')}, COMBO_END};`;
 const combo_implementation = `[${combo_enum}] = COMBO(${combo_name}, KC_${targetKey.toUpperCase()}),`
 
+console.log(`${combo_enum},`)
 console.log(combo_definition);
 console.log(combo_implementation)
