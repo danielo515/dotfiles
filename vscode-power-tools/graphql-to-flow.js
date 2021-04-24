dae0cd1edc08edcafb94326e9771a5473b044119
Danielo Rodriguez <rdanielo@gmail.com>
2021-04-24 10:23:08 +0200

    chore: small stupid improvements

--- vscode-power-tools/graphql-to-flow.js
+++ vscode-power-tools/graphql-to-flow.js
@@ -1,10 +1,13 @@
 const cleanupFields = ["form", "name", "validate"];
 const inputToSpring = (inputStr) => {
   return inputStr
-    .replace(/Int!/g, "number,")
-    .replace(/String!/g, "string,")
-    .replace(/Time!/g, "Date,")
-    .replace(/Boolean!/g, "bool,");
+    .replace(/type (\w+) /g, "type $1 = ")
+    .replace(/: Int!?/g, ": number,")
+    .replace(/String!?/g, "string,")
+    .replace(/Time!?/g, "Date,")
+    .replace(/Boolean!?/g, "bool,")
+    .replace(/: \[(\w+)!?\]/g, ": $1[]")
+    .replace(/!$/gm, ","); // remaining non recogniced types
 };
 exports.execute = async (args) => {
   // args => https://egodigital.github.io/vscode-powertools/api/interfaces/_contracts_.workspacecommandscriptarguments.html
