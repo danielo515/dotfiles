21e5f0488e57fc6c54f4bd89680918bfb3723e20
Danielo Rodriguez <rdanielo@gmail.com>
2022-04-07 11:26:57 +0200

    added a bunch of web-dev plugins

--- chezmoi/dot_config/lvim/lua/user/plugins.lua
+++ chezmoi/dot_config/lvim/lua/user/plugins.lua
@@ -159,6 +159,34 @@ lvim.plugins = {
 	-- persistence.plugin,
 	require("user.autosession"),
 	{ "kosayoda/nvim-lightbulb" },
+
+	-- auto close and rename markup tags
+	{
+		"windwp/nvim-ts-autotag",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{
+		"nvim-treesitter/playground",
+		cmd = "TSPlaygroundToggle",
+		config = function()
+			lvim.builtin.treesitter.playground.enable = true
+		end,
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "*" }, {
+				rgb_fn = true, -- CSS rgb() and rgba() functions
+				hsl_fn = true, -- CSS hsl() and hsla() functions
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+				mode = "background", -- Set the display mode.
+			})
+		end,
+	},
 }
 
 vim.list_extend(lvim.plugins, require("user.telescope").plugins)
