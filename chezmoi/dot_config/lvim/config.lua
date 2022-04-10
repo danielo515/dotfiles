4f9f936aeec615d1ab829338c66b1b0db29b2b98
Danielo Rodriguez <rdanielo@gmail.com>
2022-04-10 17:30:28 +0200

    move treesiter config to separate file

--- chezmoi/dot_config/lvim/config.lua
+++ chezmoi/dot_config/lvim/config.lua
@@ -1,11 +1,5 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
+-- generic LSP settings
+lvim.lsp.automatic_servers_installation = true
 -- general
 lvim.log.level = "debug"
 lvim.format_on_save = true
@@ -20,36 +14,12 @@ lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 1
-
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-	"bash",
-	"c",
-	"javascript",
-	"json",
-	"lua",
-	"python",
-	"typescript",
-	"tsx",
-	"css",
-	"rust",
-	"java",
-	"yaml",
-}
-
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
-
--- generic LSP settings
-lvim.lsp.automatic_servers_installation = true
+lvim.builtin.which_key.setup.plugins.presets.motions = true
 
 -- You will likely want to reduce updatetime which affects CursorHold
 -- note: this setting is global and should be set only once
 vim.o.updatetime = 250
 vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
-
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
 
 -- hot reload
 for module, _ in pairs(package.loaded) do
@@ -66,3 +36,4 @@ require("user.autocommands").configure()
 require("user.linters").config()
 -- require("user.lualine").config()
 require("luasnip.loaders.from_snipmate").lazy_load()
+require("user.tree-sitter")
