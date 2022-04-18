5fbad46822a336b4ffb0b2351c8b9a1039365fa0
Danielo Rodriguez <rdanielo@gmail.com>
2022-04-18 11:43:56 +0200

    alternative harpoon mappings

--- chezmoi/dot_config/lvim/lua/user/harpoon.lua
+++ chezmoi/dot_config/lvim/lua/user/harpoon.lua
@@ -4,6 +4,7 @@ lvim.keys.normal_mode["te"] = "<cmd>lua require('harpoon.term').gotoTerminal(2)<
 lvim.keys.normal_mode["cu"] = "<cmd>lua require('harpoon.term').sendCommand(1, 1)<CR>"
 lvim.keys.normal_mode["ce"] = "<cmd>lua require('harpoon.term').sendCommand(1, 2)<CR>"
 lvim.keys.normal_mode["<M-Space>"] = "<cmd>lua require('harpoon.cmd-ui').toggle_quick_menu()<CR>"
+lvim.keys.normal_mode["mh"] = "<cmd>lua require('harpoon.mark').add_file()<cr>"
 
 M.plugin = {
 	"ThePrimeagen/harpoon",
