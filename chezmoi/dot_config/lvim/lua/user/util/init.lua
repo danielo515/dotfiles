83d190e45a6fa468a850a715ee2e242381900de0
Danielo Rodriguez <rdanielo@gmail.com>
2022-04-27 10:34:25 +0200

    feat: added function to resize window

--- chezmoi/dot_config/lvim/lua/user/util/init.lua
+++ chezmoi/dot_config/lvim/lua/user/util/init.lua
@@ -9,4 +9,26 @@ function M.concat_lists(...)
 	return result
 end
 
+-- Resizes the current window to the maximum required width
+function M.resize_window()
+	local getOpt = nvim.api.nvim_win_get_option
+	local lines_count = vim.api.nvim_buf_line_count(0)
+	local lines = vim.api.nvim_buf_get_lines(0, 0, lines_count, false)
+	local numbers_active = getOpt(0, "rnu") or getOpt(0, "number")
+	local numwidth = numbers_active and getOpt(0, "nuw") or 0
+	local padding = numwidth + 1
+
+	local longerLine = 0
+	for _, line in ipairs(lines) do
+		local new_len = string.len(line) or 0
+		if new_len > longerLine then
+			longerLine = new_len
+		end
+	end
+	local cmd = (longerLine + padding) .. "wincmd |"
+	vim.api.nvim_command(cmd)
+end
+
+vim.cmd('command! ResizeWindow :lua require("user.util").resize_window()')
+
 return M
