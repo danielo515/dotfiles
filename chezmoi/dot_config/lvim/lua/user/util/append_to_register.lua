54f14f86af516fa23c596f0d80d830ff81389552
Danielo Rodriguez <rdanielo@gmail.com>
2022-05-10 10:55:05 +0200

    feat: function to append strings to register as object notation

--- chezmoi/dot_config/lvim/lua/user/util/append_to_register.lua
+++ chezmoi/dot_config/lvim/lua/user/util/append_to_register.lua
@@ -5,4 +5,27 @@ function M.as_array()
 	vim.fn.setreg("*", "'" .. word .. "', ", "a")
 end
 
+local function add_to_reg(reg, text)
+	vim.fn.setreg(reg, text, "a")
+end
+
+local ts = require("nvim-treesitter.ts_utils")
+local isKey = true
+-- Appends the current string under cursor to a registry with object notation
+function M.as_object(reg)
+	reg = reg or "*"
+	local node = ts.get_node_at_cursor()
+	local node_type = node:type()
+	if not node_type:find("string") then
+		vim.notify("This function only works with cursor at strings", "warn")
+		return
+	end
+	local text = ts.get_node_text(node)[1]
+	print(node_type, text)
+	local wrap = isKey and (not text:find('"')) and '"' or ""
+	local separator = isKey and ": " or ", "
+	isKey = not isKey
+	add_to_reg(reg, wrap .. text .. wrap .. separator)
+end
+
 return M
