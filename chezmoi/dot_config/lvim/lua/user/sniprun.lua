6a90706b317638038379bcfc88094c0a12edce2a
Danielo Rodriguez <rdanielo@gmail.com>
2022-05-12 17:02:57 +0200

    chore: configure sniprun

--- chezmoi/dot_config/lvim/lua/user/sniprun.lua
+++ chezmoi/dot_config/lvim/lua/user/sniprun.lua
@@ -1,5 +1,6 @@
 local status_ok, sniprun = pcall(require, "sniprun")
 if not status_ok then
+	vim.notify("Sniprun not loaded", "warn")
 	return
 end
 
@@ -19,12 +20,12 @@ sniprun.setup({
 	display = {
 		-- "Classic", --# display results in the command-line  area
 		"VirtualTextOk", --# display ok results as virtual text (multiline is shortened)
-		-- "VirtualTextErr", --# display error results as virtual text
+		"VirtualTextErr", --# display error results as virtual text
 		-- "TempFloatingWindow",      --# display results in a floating window
 		-- "LongTempFloatingWindow",  --# same as above, but only long results. To use with VirtualText__
 		-- "Terminal",                --# display results in a vertical split
 		-- "TerminalWithCode",        --# display results and code history in a vertical split
-		-- "NvimNotify",              --# display with the nvim-notify plugin
+		"NvimNotify", --# display with the nvim-notify plugin
 		-- "Api"                      --# return output to a programming interface
 	},
 
