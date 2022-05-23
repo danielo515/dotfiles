40f35ec87281bcb2a644f2f3ed558d9799643b9c
Danielo Rodriguez <rdanielo@gmail.com>
2022-05-23 08:09:22 +0200

    feat: integrate matchup with treesitter

--- chezmoi/dot_config/lvim/lua/user/plugins.lua
+++ chezmoi/dot_config/lvim/lua/user/plugins.lua
@@ -19,8 +19,20 @@ local plugins = {
 	{
 		"andymass/vim-matchup",
 		event = "CursorMoved",
+		after = "nvim-treesitter",
 		config = function()
 			vim.g.matchup_matchparen_offscreen = { method = "popup" }
+			local treeOk = pcall(function()
+				require("nvim-treesitter.configs").setup({
+					matchup = {
+						enable = true, -- mandatory, false will disable the whole extension
+						disable = { "c", "ruby" }, -- optional, list of language that will be disabled
+					},
+				})
+			end)
+			if not treeOk then
+				vim.notify("Can not attach matchup to treesitter")
+			end
 		end,
 	},
 	-- explore LSP symbols
