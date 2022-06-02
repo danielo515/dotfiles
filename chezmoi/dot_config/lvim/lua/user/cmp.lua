d16ae79baf80b69f52f90ffd575166bef0cea6c8
Danielo Rodriguez <rdanielo@gmail.com>
2022-06-02 11:33:31 +0200

    refactor cmp file

--- chezmoi/dot_config/lvim/lua/user/cmp.lua
+++ chezmoi/dot_config/lvim/lua/user/cmp.lua
@@ -1,19 +1,24 @@
 -- CMP specific plugins and configruations
-table.insert(lvim.builtin.cmp.sources, {
-  name = "rg",
-})
-table.insert(lvim.builtin.cmp.sources, { name = "npm", keyword_length = 4 })
-table.insert(lvim.builtin.cmp.sources, {
+local sources = {
+  { name = "npm", keyword_length = 4 },
   {
-    name = "tmux",
-    option = {
-      all_panes = false,
-      label = "[tmux]",
-      trigger_characters = { "." },
-      trigger_characters_ft = {}, -- { filetype = { '.' } }
+    {
+      name = "tmux",
+      option = {
+        all_panes = true,
+        label = "[tmux]",
+        trigger_characters = { "." },
+        trigger_characters_ft = {}, -- { filetype = { '.' } }
+      },
     },
   },
-})
+  { name = "rg" },
+  { name = "emoji" },
+}
+
+for _, source in ipairs(sources) do
+  table.insert(lvim.builtin.cmp.sources, source)
+end
 
 return {
   {
@@ -28,4 +33,5 @@ return {
       "nvim-lua/plenary.nvim",
     },
   },
+  { "hrsh7th/cmp-emoji" },
 }
--- chezmoi/dot_config/lvim/lua/user/statusline.lua
+++ chezmoi/dot_config/lvim/lua/user/statusline.lua
@@ -21,6 +21,35 @@ local nvim_gps = function()
   end
 end
 
+local windows = {
+  "windows",
+  show_filename_only = true, -- Shows shortened relative path when set to false.
+  show_modified_status = true, -- Shows indicator when the window is modified.
+
+  mode = 0, -- 0: Shows window name
+  -- 1: Shows window index
+  -- 2: Shows window name + window index
+
+  -- max_length = vim.o.columns * 2 / 3, -- Maximum width of windows component,
+  -- it can also be a function that returns
+  -- the value of `max_length` dynamically.
+  filetype_names = {
+    TelescopePrompt = "Telescope",
+    dashboard = "Dashboard",
+    packer = "Packer",
+    fzf = "FZF",
+    alpha = "Alpha",
+  }, -- Shows specific window name for that filetype ( { `filetype` = `window_name`, ... } )
+
+  disabled_buftypes = { "quickfix", "NvimTree" }, -- Hide a window if its buffer's type is disabled
+
+  windows_color = {
+    -- Same values as the general color option can be used here.
+    active = "lualine_{section}_normal", -- Color for active window.
+    inactive = "lualine_{section}_inactive", -- Color for inactive window.
+  },
+}
+
 lvim.builtin.lualine.options = {
   theme = "auto", -- lualine theme
   component_separators = { left = "", right = "" },
@@ -42,7 +71,7 @@ lvim.builtin.lualine.sections.lualine_a = {
     -- 1: Relative path
     -- 2: Absolute path
 
-    shorting_target = 60, -- Shortens path to leave 40 spaces in the window
+    shorting_target = 60, -- Shortens path to leave 60 spaces in the window
     symbols = {
       modified = "[+]", -- Text to show when the file is modified.
       readonly = "[!]", -- Text to show when the file is non-modifiable or readonly.
@@ -50,6 +79,7 @@ lvim.builtin.lualine.sections.lualine_a = {
     },
   },
 }
+
 lvim.builtin.lualine.sections.lualine_b = { components.branch, "windows" }
 lvim.builtin.lualine.sections.lualine_c = {
   components.diff,
