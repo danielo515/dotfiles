e7ffb1ecb1a988180998d2eec78faeca614acaf3
Danielo Rodriguez <rdanielo@gmail.com>
2022-06-07 15:12:34 +0200

    chore: wich key stuff

--- chezmoi/dot_config/lvim/lua/user/which-key.lua
+++ chezmoi/dot_config/lvim/lua/user/which-key.lua
@@ -13,12 +13,14 @@ end
 lvim.builtin.which_key.setup.plugins.presets = {
   operators = false, -- adds help for operators like d, y, ...
   motions = false, -- adds help for motions
-  text_objects = false, -- help for text objects triggered after entering an operator
+  text_objects = true, -- help for text objects triggered after entering an operator
   windows = true, -- default bindings on <c-w>
   nav = true, -- misc bindings to work with windows
   z = true, -- bindings for folds, spelling and others prefixed with z
   g = true, -- bindings for prefixed with g
 }
+
+lvim.builtin.which_key.mappings.q = nil
 
 -- This config will be merged with the one that lvim has by default
 local whichConfig = {
