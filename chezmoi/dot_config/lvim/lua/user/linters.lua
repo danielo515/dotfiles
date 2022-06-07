a651222beed41a0ce801e4f35e1d6558dddddb71
Danielo Rodriguez <rdanielo@gmail.com>
2022-06-07 07:54:26 +0200

    chore: comment out stylua for now

--- chezmoi/dot_config/lvim/lua/user/linters.lua
+++ chezmoi/dot_config/lvim/lua/user/linters.lua
@@ -1,31 +1,31 @@
 local M = {}
 
 function M.config()
-	local formatters = require("lvim.lsp.null-ls.formatters")
-	formatters.setup({
-		{
-			name = "eslint_d",
-		},
-		{ name = "stylua" },
-	})
+  local formatters = require "lvim.lsp.null-ls.formatters"
+  formatters.setup {
+    {
+      name = "eslint_d",
+    },
+    -- { name = "stylua" },
+  }
 
-	local linters = require("lvim.lsp.null-ls.linters")
-	linters.setup({
-		{ command = "eslint", filetypes = { "typescript", "typescriptreact" } },
-	})
-	local graphql_lsp_opts = {
-		filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript" },
-	}
+  local linters = require "lvim.lsp.null-ls.linters"
+  linters.setup {
+    { command = "eslint", filetypes = { "typescript", "typescriptreact" } },
+  }
+  local graphql_lsp_opts = {
+    filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript" },
+  }
 
-	require("lvim.lsp.manager").setup("graphql", graphql_lsp_opts)
-	require("lvim.lsp.manager").setup("jsonls", {
-		settings = {
-			json = {
-				schemas = require("schemastore").json.schemas(),
-				validate = { enable = true },
-			},
-		},
-	})
+  require("lvim.lsp.manager").setup("graphql", graphql_lsp_opts)
+  require("lvim.lsp.manager").setup("jsonls", {
+    settings = {
+      json = {
+        schemas = require("schemastore").json.schemas(),
+        validate = { enable = true },
+      },
+    },
+  })
 end
 
 return M
