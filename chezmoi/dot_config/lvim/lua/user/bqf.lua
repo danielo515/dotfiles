b892f113bbf6035bf6629052e0ee9c503904536d
Danielo Rodriguez <rdanielo@gmail.com>
2022-06-08 09:10:01 +0200

    some bqf improvements

--- chezmoi/dot_config/lvim/lua/user/bqf.lua
+++ chezmoi/dot_config/lvim/lua/user/bqf.lua
@@ -1,49 +1,50 @@
 local M = {}
 
 M.config = function()
-	local status_ok, bqf = pcall(require, "bqf")
-	if not status_ok then
-		return
-	end
+  local status_ok, bqf = pcall(require, "bqf")
+  if not status_ok then
+    return
+  end
 
-	bqf.setup({
-		auto_resize_height = true,
-		func_map = {
-			tab = "st",
-			split = "s",
-			vsplit = "v",
+  bqf.setup({
+    auto_resize_height = true,
+    func_map = {
+      tab = "st",
+      split = "s",
+      vsplit = "v",
 
-			stoggleup = "K",
-			stoggledown = "J",
-			stogglevm = "<Space>",
+      stoggleup = "K",
+      stoggledown = "J",
+      stogglevm = "x",
 
-			ptoggleitem = "p",
-			ptoggleauto = "P",
-			ptogglemode = "zp",
-
-			pscrollup = "<C-b>",
-			pscrolldown = "<C-f>",
-
-			prevfile = "gk",
-			nextfile = "gj",
-
-			prevhist = "<S-Tab>",
-			nexthist = "<Tab>",
-		},
-		preview = {
-			auto_preview = true,
-			should_preview_cb = function(bufnr)
-				local ret = true
-				local filename = vim.api.nvim_buf_get_name(bufnr)
-				local fsize = vim.fn.getfsize(filename)
-				-- file size greater than 10k can't be previewed automatically
-				if fsize > 100 * 1024 then
-					ret = false
-				end
-				return ret
-			end,
-		},
-	})
+      ptoggleitem = "zp",
+      ptoggleauto = "P",
+      -- preview fullscreen
+      ptogglemode = "p",
+      -- scroll preview
+      pscrollup = "u",
+      pscrolldown = "d",
+      -- goes to the next entry in a different file
+      prevfile = "gk",
+      nextfile = "gj",
+      -- previous qf hihstory
+      prevhist = "<S-Tab>",
+      nexthist = "<Tab>",
+    },
+    preview = {
+      auto_preview = true,
+      should_preview_cb = function(bufnr)
+        local ret = true
+        local filename = vim.api.nvim_buf_get_name(bufnr)
+        local fsize = vim.fn.getfsize(filename)
+        -- file size greater than 10k can't be previewed automatically
+        if fsize > 100 * 1024 then
+          ret = false
+        end
+        return ret
+      end,
+    },
+  })
 end
 
 return M
