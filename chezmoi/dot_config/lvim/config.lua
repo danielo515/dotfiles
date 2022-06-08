8e98fcc46b932bdb0a123ad0d9eae74038c348cf
Danielo Rodriguez <rdanielo@gmail.com>
2022-06-08 14:32:37 +0200

    move nvimtree config to separate file

--- chezmoi/dot_config/lvim/config.lua
+++ chezmoi/dot_config/lvim/config.lua
@@ -23,49 +23,6 @@ lvim.builtin.alpha.mode = "custom"
 local alpha_opts = require("user.dashboard").config()
 lvim.builtin.alpha["custom"] = { config = alpha_opts }
 
--- Nvimtree
--- =========================================
-lvim.builtin.nvimtree.setup.open_on_setup = false
-lvim.builtin.nvimtree.setup.view.auto_resize = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 1
-lvim.builtin.nvimtree.setup.renderer = {
-  indent_markers = {
-    enable = true,
-    icons = {
-      corner = "└ ",
-      edge = "│ ",
-      none = "  ",
-    },
-  },
-}
-local function telescope_find_files(_)
-  require("lvim.core.nvimtree").start_telescope "find_files"
-end
-
-local function telescope_live_grep(_)
-  require("lvim.core.nvimtree").start_telescope "live_grep"
-end
-
-end
-
-lvim.builtin.nvimtree.setup.view.mappings.list = {
-  { key = { "l", "<CR>" }, action = "edit", mode = "n" },
-  { key = "h", action = "close_node" },
-  { key = "v", action = "vsplit" },
-  { key = "C", action = "cd" },
-  { key = "o", action = "edit_no_picker" },
-  { key = "<C-F>", action = "live_filter" },
-  { key = "f", action = "telescope_find_files", action_cb = telescope_find_files },
-  { key = "gr", action = "telescope_live_grep", action_cb = telescope_live_grep },
-}
-
-lvim.builtin.nvimtree.setup.hijack_directories.ignore_list = { "help" }
-lvim.builtin.nvimtree.setup.diagnostics.show_on_dirs = true
-lvim.builtin.nvimtree.setup.filters.custom = { "\\.cache" }
--- Nvimtree end
--- =========================================
-
 vim.wo.relativenumber = true
 -- This is required for vim-surround, otherwise it is too fast
 vim.o.timeoutlen = 500
@@ -92,6 +49,7 @@ require "user.telescope"
 require("user.autocommands").config()
 require "user.statusline"
 require "user.sniprun"
+require "user.builtin.nvimtree"
 require("user.linters").config()
 require("user.auto-resize-window").setup()
 -- require("user.lualine").config()
---
+++ chezmoi/dot_config/lvim/lua/user/builtin/nvimtree.lua
@@ -0,0 +1,49 @@
+-- Nvimtree
+-- =========================================
+lvim.builtin.nvimtree.setup.open_on_setup = false
+lvim.builtin.nvimtree.setup.view.auto_resize = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 1
+lvim.builtin.nvimtree.setup.renderer = {
+  indent_markers = {
+    enable = true,
+    icons = {
+      corner = "└ ",
+      edge = "│ ",
+      none = "  ",
+    },
+  },
+}
+local function telescope_find_files(_)
+  require("lvim.core.nvimtree").start_telescope "find_files"
+end
+
+local function telescope_live_grep(_)
+  require("lvim.core.nvimtree").start_telescope "live_grep"
+end
+
+local function handle_single_click(node)
+  local open = require("nvim-tree.actions.open-file").fn
+  vim.cmd [[
+  execute "normal! \<LeftMouse>"
+  ]]
+end
+
+lvim.builtin.nvimtree.setup.view.mappings.list = {
+  { key = { "l", "<CR>" }, action = "edit", mode = "n" },
+  { key = "h", action = "close_node" },
+  { key = "v", action = "vsplit" },
+  { key = "C", action = "cd" },
+  { key = "o", action = "edit_no_picker" },
+  { key = "<C-F>", action = "live_filter" },
+  { key = "f", action = "telescope_find_files", action_cb = telescope_find_files },
+  { key = "gr", action = "telescope_live_grep", action_cb = telescope_live_grep },
+  -- { key = "<2-LeftMouse>", action = "" },
+  { key = "<LeftMouse>", action = "open node", action_cb = handle_single_click },
+}
+
+lvim.builtin.nvimtree.setup.hijack_directories.ignore_list = { "help" }
+lvim.builtin.nvimtree.setup.diagnostics.show_on_dirs = true
+lvim.builtin.nvimtree.setup.filters.custom = { "\\.cache" }
+-- Nvimtree end
+-- =========================================
