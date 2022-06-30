5ad70b51b1a02d57f77c6263b9df4d8c9b2edd1f
Danielo Rodriguez <rdanielo@gmail.com>
2022-06-30 11:54:14 +0200

    chore: more scm queries

--- chezmoi/dot_config/lvim/after/queries/javascript/textobjects.scm
+++ chezmoi/dot_config/lvim/after/queries/javascript/textobjects.scm
@@ -1,2 +1,4 @@
 (variable_declarator (identifier) @variable)
 (variable_declarator (object_pattern (shorthand_property_identifier_pattern) @variable))
+(object_pattern (shorthand_property_identifier_pattern) @variable)
+(shorthand_property_identifier)@parameter.inner
