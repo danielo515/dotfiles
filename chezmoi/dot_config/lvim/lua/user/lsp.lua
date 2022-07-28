6d83fe9581ba8b35e6dfcab06cbd0a5fa01f38e7
Danielo Rodriguez <rdanielo@gmail.com>
2022-07-28 11:13:38 +0200

    chore: refactoring as null-ls source

--- chezmoi/dot_config/lvim/lua/user/lsp.lua
+++ chezmoi/dot_config/lvim/lua/user/lsp.lua
@@ -1,12 +1,15 @@
 local M = {}
 
+local status_ok, nls = pcall(require, "null-ls")
+if not status_ok then
+  return
+end
+
 function M.config()
   local formatters = require "lvim.lsp.null-ls.formatters"
   formatters.setup {
-    {
-      name = "eslint_d",
-    },
-    -- { name = "stylua" },
+    { name = "eslint_d" },
+    { name = "stylua" },
   }
 
   local linters = require "lvim.lsp.null-ls.linters"
@@ -18,7 +21,7 @@ function M.config()
   }
 
   require("lvim.lsp.manager").setup("graphql", graphql_lsp_opts)
-  require("lvim.lsp.manager").setup("marksman")
+  require("lvim.lsp.manager").setup "marksman"
   require("lvim.lsp.manager").setup("jsonls", {
     settings = {
       json = {
@@ -27,6 +30,11 @@ function M.config()
       },
     },
   })
+  local setup_code_actions = require("lvim.lsp.null-ls.code_actions").setup
+  local refactorin_opts = nls.builtins.code_actions.refactoring.with {
+    filetypes = { "typescript", "javascript", "lua", "c", "cpp", "go", "python", "java", "php" },
+  }
+  setup_code_actions { refactorin_opts }
 end
 
 return M
