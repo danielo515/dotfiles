fb5d220df67fcfbafb638a181ea9e0a588f5a7c6
Danielo Rodriguez <rdanielo@gmail.com>
2022-08-12 18:02:33 +0200

    feat: awesome swap windows mapping

--- chezmoi/dot_config/lvim/lua/user/keymaps.lua
+++ chezmoi/dot_config/lvim/lua/user/keymaps.lua
@@ -77,3 +77,16 @@ vim.keymap.set("n", ",w", function()
   }) or vim.api.nvim_get_current_win()
   vim.api.nvim_set_current_win(picked_window_id)
 end, { desc = "Pick a window" })
+-- Swap two windows using the awesome window picker
+local function swap_windows()
+  local window = picker.pick_window({
+    include_current_win = false
+  })
+  local target_buffer = vim.fn.winbufnr(window)
+  -- Set the target window to contain current buffer
+  vim.api.nvim_win_set_buf(window, 0)
+  -- Set current window to contain target buffer
+  vim.api.nvim_win_set_buf(0, target_buffer)
+end
+
+nmap(',W', swap_windows, 'Swap windows')
