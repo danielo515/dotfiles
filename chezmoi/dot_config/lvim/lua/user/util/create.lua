73515db66b40c0755aa1fb90849f96b2118a56a0
Danielo Rodriguez <rdanielo@gmail.com>
2022-08-16 11:42:51 +0200

    fix: create sibling

--- chezmoi/dot_config/lvim/lua/user/util/create.lua
+++ chezmoi/dot_config/lvim/lua/user/util/create.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.create_sibling_file()
   local path = vim.fn.expand "%:p:h"
   local filename = vim.fn.inputdialog("New file name> ", "")
-  vim.cmd("e: " .. path .. "/" .. filename)
+  vim.cmd("e " .. path .. "/" .. filename)
 end
 
 return M
