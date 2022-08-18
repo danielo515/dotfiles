f6310cea66263ae7b2f423f0e597eb5f7a651409
Danielo Rodriguez <rdanielo@gmail.com>
2022-08-18 12:59:21 +0200

    chore: toggle lsp lines

--- chezmoi/dot_config/lvim/lua/user/plugins/lsp-lines.lua
+++ chezmoi/dot_config/lvim/lua/user/plugins/lsp-lines.lua
@@ -1,6 +1,12 @@
 -- Disable virtual text
 lvim.lsp.diagnostics.virtual_text = false
 lvim.builtin.which_key.mappings["gj"] = { "<cmd>lua require('lsp_lines').toggle()<cr>", "Toggle LSP lines" }
+local bind_command = require("legendary").bind_command
+bind_command {
+  ":ToggleLspLines",
+  "<cmd>lua require('lsp_lines').toggle()<cr>",
+  description = "toggle lsp lines",
+}
 -- Configure the installation
 return {
   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
