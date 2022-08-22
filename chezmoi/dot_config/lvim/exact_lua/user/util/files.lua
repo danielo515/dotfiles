7d9670f34bba34e9f6f9fb0bf70cb33349930f76
Danielo Rodriguez <rdanielo@gmail.com>
2022-08-22 12:40:14 +0200

    fix: inject plugin commit now working

--- chezmoi/dot_config/lvim/exact_lua/user/util/files.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/util/files.lua
@@ -1,9 +1,20 @@
 local Log = require "lvim.core.log"
 
+local function is_file_ok(filePath)
+  if vim.fn.filereadable(filePath) == 0 then
+    Log:error("The provided file path does not exist: " .. filePath)
+    return false
+  end
+  return true
+end
+
 --- Given a file path, reads it as json file
 ---@param path string
----@return table
+---@return table | nil
 local function read_json_file(path)
+  if not is_file_ok(path) then
+    return nil
+  end
   local content = vim.fn.readfile(path)
   local data = vim.fn.json_decode(content)
   return data
@@ -14,8 +25,7 @@ end
 ---@return nil | table
 local function read_local_json_file(filename)
   local filePath = join_paths(vim.fn.getcwd(), filename)
-  if not vim.fn.filereadable(filePath) then
-    Log:error("The provided file does not exist: " .. filePath)
+  if not is_file_ok(filePath) then
     return nil
   end
   local data = read_json_file(filePath)
@@ -24,8 +34,7 @@ end
 
 local function read_relative_json_file(relative_path)
   local filePath = join_paths(vim.fn.expandcmd "%:p:h", relative_path)
-  if not vim.fn.filereadable(filePath) then
-    Log:error("The provided file path does not exist: " .. filePath)
+  if not is_file_ok(filePath) then
     return nil
   end
   local data = read_json_file(filePath)
@@ -36,4 +45,5 @@ return {
   read_relative_json_file = read_relative_json_file,
   read_local_json_file = read_local_json_file,
   read_json_file = read_json_file,
+  is_file_ok = is_file_ok,
 }
--- chezmoi/dot_config/lvim/exact_lua/user/util/plugins.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/util/plugins.lua
@@ -1,31 +1,47 @@
 local u = require "user.util.files"
 local log = require "lvim.core.log"
-local default_sha1 = u.read_relative_json_file "../../../snapshots/default.json"
-if default_sha1 == nil then
-  log:debug "Can't load plugins snapshot"
-  return
-end
 
-local get_default_sha1 = function(spec)
+local get_default_sha1 = function(spec, default_sha1)
   local short_name, _ = require("packer.util").get_plugin_short_name(spec)
   return default_sha1[short_name] and default_sha1[short_name].commit
 end
 
---- Injects the commits that are part of the packer snapshot into the provided list of plugins
+---Performs the real injection, recursively if needed
 ---@param plugins_list table
-local function inject_snapshot_commit(plugins_list)
-  return vim.tbl_map(function(spec)
+---@param default_sha1 object
+---@return table
+local function inject_rec(plugins_list, default_sha1)
+  local result = vim.tbl_map(function(spec)
     if type(spec) ~= "table" then
-      log:debug("There is an entry that is not a table in the plugins list: " .. spec, { title = "Danielo" })
+      log:debug(
+        "There is an entry that is not a table in the plugins list: " .. vim.inspect(spec),
+        { title = "Danielo" }
+      )
       spec = { spec, commit = "" }
     end
-    spec["commit"] = get_default_sha1(spec)
+    if type(spec[1]) == "table" then
+      spec = inject_rec(spec, default_sha1)
+    else
+      spec["commit"] = get_default_sha1(spec, default_sha1)
+    end
     return spec
   end, plugins_list)
+  return result
 end
 
-vim.pretty_print(inject_snapshot_commit(require "user.plugins"))
-
+--- Injects the commits that are part of the packer snapshot into the provided list of plugins
+---@param plugins_list table
+---@param snapshot_path string the path of the snapshot where you want to extract commits from
+---@return table
+local function inject_snapshot_commit(plugins_list, snapshot_path)
+  local default_sha1 = u.read_json_file(snapshot_path)
+  log:debug("list of plugins read? " .. vim.inspect(default_sha1 ~= nil))
+  if default_sha1 == nil then
+    log:warn "Can't load plugins snapshot"
+    return plugins_list
+  end
+  return inject_rec(plugins_list, default_sha1)
+end
 return {
   inject_snapshot_commit = inject_snapshot_commit,
 }
