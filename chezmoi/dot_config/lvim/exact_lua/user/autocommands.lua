391b412142fc843212d3c263cf3b2b28d1ec15fa
Danielo Rodriguez <rdanielo@gmail.com>
2022-08-22 12:39:55 +0200

    fix: improvements to autocommands

--- chezmoi/dot_config/lvim/exact_lua/user/autocommands.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/autocommands.lua
@@ -3,7 +3,6 @@ local M = {}
 -- You will likely want to reduce updatetime which affects CursorHold
 -- note: this setting is global and should be set only once
 vim.o.updatetime = 250
-vim.cmd [[autocmd! CursorHold * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
 local function reloadSnippets()
   require("luasnip").cleanup() -- opts can be ommited
@@ -24,6 +23,7 @@ vim.api.nvim_create_autocmd({
 })
 
 local function add_node_bin()
+  ---@diagnostic disable-next-line: missing-parameter
   local binPath = vim.fn.glob "./node_modules/.bin"
   if binPath ~= "" then
     vim.env.PATH = vim.env.PATH .. ":" .. binPath
@@ -34,7 +34,7 @@ end
 function M.config()
   local get_chezmoi_dir = require("user.util.chezmoi").get_chezmoi_dir
   local codelens_viewer = "lua require('nvim-lightbulb').update_lightbulb()"
-  autocommands = {
+  local autocommands = {
     -- Apply chezmoi whenever a dotfile is updated
     {
       "BufWritePost",
@@ -55,14 +55,14 @@ function M.config()
     -- { "Filetype", "typescript,typescriptreact", "nnoremap gx <Cmd>TypescriptFixAll<CR>" },
 
     -- Populate node_modules .bin
-    { "VimEnter", "", add_node_bin },
+    { "VimEnter", "", add_node_bin, "Add the local node_modules to the vim PATH" },
     -- uncomment the following if you want to show diagnostics on hover
-    -- { "CursorHold", "*", "lua vim.diagnostic.open_float()" },
+    { "CursorHold", "*", "lua vim.diagnostic.open_float()" },
   }
   local group = vim.api.nvim_create_augroup("Danielo", {})
   for _, command in ipairs(autocommands) do
-    local name, pattern, cmd = unpack(command)
-    local options = { pattern = pattern, group = group }
+    local name, pattern, cmd, desc = unpack(command)
+    local options = { pattern = pattern, group = group, desc = desc }
 
     if type(cmd) == "string" then
       options.command = cmd
