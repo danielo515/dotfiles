43c81887c5c4977ce5051c8e879712c663523353
Danielo Rodriguez <rdanielo@gmail.com>
2022-08-26 10:42:50 +0200

    feat: snippet snippets

--- chezmoi/dot_config/lvim/luasnippets/lua.lua
+++ chezmoi/dot_config/lvim/luasnippets/lua.lua
@@ -1,3 +1,6 @@
+local extras = require "luasnip.extras"
+local rep = extras.rep
+
 return {
   s(
     { trig = "pc", dscr = "protected call" },
@@ -7,5 +10,13 @@ return {
       i(3),
       i(4),
     })
+  ),
+  s(
+    { trig = "snip", dscr = "Create lua snippet" },
+    fmt([[ s({{ trig = "{trig}", dscr = "{dscr}" }}, {} )]], { trig = i(1), dscr = i(2), i(0) })
+  ),
+  s(
+    { trig = "fmt", dscr = "fmt snippet to use with snip" },
+    fmt([[ fmt( "{}" , {{ {} = i({}) }}) )]], { i(1, "key"), rep(1), i(2, "value") })
   ),
 }
