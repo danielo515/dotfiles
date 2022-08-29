94d83872b1360d7965024b99187a970194c873e4
Danielo Rodriguez <rdanielo@gmail.com>
2022-08-29 17:14:41 +0200

    chore: document toggle_dev

--- chezmoi/private_dot_local/private_share/lunarvim/executable_toggle_dev.sh
+++ chezmoi/private_dot_local/private_share/lunarvim/executable_toggle_dev.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Toggle between using normal lunarvim installation
+# and your local clone.
+# This script is very basic and dumb.
+# It relies in the existence of a folder (or a symlink to a folder)
+# in the same directory that contains or points to your local clone
+# Such folder/link must be named lvim_dev
 cd ~/.local/share/lunarvim/ || exit
 if test -d lvim_dev; then
   echo "Entering dev mode"
