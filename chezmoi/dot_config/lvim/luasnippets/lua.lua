db4b8a5b31a64aae98ead3698b44abf577f00a51
Danielo Rodriguez <rdanielo@gmail.com>
2022-08-30 10:40:33 +0200

    feat: more awesome snippets

--- chezmoi/dot_config/lvim/luasnippets/lua.lua
+++ chezmoi/dot_config/lvim/luasnippets/lua.lua
@@ -1,5 +1,8 @@
+local ls = require "luasnip"
 local extras = require "luasnip.extras"
 local rep = extras.rep
+local fmta = require("luasnip.extras.fmt").fmta
+local t = ls.text_node
 
 return {
   s(
@@ -11,12 +14,22 @@ return {
       i(4),
     })
   ),
+  s({ trig = "vpp", dscr = "Vim pretty print" }, { t "vim.pretty_print(", i(1), t ")" }),
   s(
     { trig = "snip", dscr = "Create lua snippet" },
-    fmt([[ s({{ trig = "{trig}", dscr = "{dscr}" }}, {} )]], { trig = i(1), dscr = i(2), i(0) })
+    fmt(
+      [[ s({{ trig = "{trig}", dscr = "{dscr}", {regTrig} }}, {} )]],
+      { trig = i(1), dscr = i(2, "description"), regTrig = c(3, { t "", t "regTrig=true" }), i(0) }
+    )
   ),
   s(
     { trig = "fmt", dscr = "fmt snippet to use with snip" },
     fmta([[ fmt( "{<>}" , { <> = i(<>,"default") }) )]], { i(1, "key"), rep(1), i(2, "1") })
   ),
+  s({ trig = "(%w+)tmap", dscr = "Wrap current table with a map method", regTrig = true }, {
+    t "vim.tbl_map(",
+    l(l.CAPTURE1, {}),
+    i(0),
+    t ")",
+  }),
 }
