c0730ad08591c9904d65057dee858e7d00699140
Danielo Rodriguez <rdanielo@gmail.com>
2022-08-30 20:03:48 +0200

    chore: more filetypes

--- chezmoi/dot_config/lvim/exact_lua/user/plugins/filetype-config.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/plugins/filetype-config.lua
@@ -3,8 +3,24 @@ return {
   config = function()
     require("filetype").setup {
       overrides = {
+        literal = {
+          ["kitty.conf"] = "kitty",
+          [".gitignore"] = "conf",
+        },
+        complex = {
+          [".clang*"] = "yaml",
+          [".*%.env.*"] = "sh",
+          [".*ignore"] = "conf",
+        },
         extensions = {
           zsh = "bash", -- used for zsh configuration files
+          tf = "terraform",
+          tfvars = "terraform",
+          hcl = "hcl",
+          tfstate = "json",
+          eslintrc = "json",
+          prettierrc = "json",
+          mdx = "markdown",
         },
       },
     }
