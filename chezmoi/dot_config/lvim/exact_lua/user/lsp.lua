37a71481f208992c9f5c5254df697393afb75eed
Danielo Rodriguez <rdanielo@gmail.com>
2022-10-03 11:48:34 +0200

    feat: add a bunch of nice things to null-ls

--- chezmoi/dot_config/lvim/exact_lua/user/lsp.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/lsp.lua
@@ -7,20 +7,49 @@ if not status_ok then
   return
 end
 
+local function check_binary(binary_name)
+  if vim.fn.executable(binary_name) == 1 then
+    -- All good
+    return
+  end
+  vim.notify("Missing required binary: " .. binary_name, vim.log.levels.ERROR)
+end
+
 lvim.lsp.automatic_configuration.skipped_filetypes = { "reason", "rescript" }
 -- lvim.lsp.automatic_configuration.skipped_servers = { "rescriptls" }
 
 function M.config()
+  --#region null-ls
+  -- https://www.lunarvim.org/languages/#linting-formatting
+
+  check_binary "codespell"
+  check_binary "eslint_d"
+  check_binary "stylua"
+  check_binary "hadolint"
+  check_binary "fixjson"
+
+  -- Just use the default null-ls config by referencing the formatters by name
   local formatters = require "lvim.lsp.null-ls.formatters"
   formatters.setup {
     { name = "eslint_d" },
     { name = "stylua" },
+    { name = "fixjson" },
   }
 
   local linters = require "lvim.lsp.null-ls.linters"
   linters.setup {
     { command = "eslint_d", filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" } },
+    { name = "codespell" },
+    { name = "hadolint" },
   }
+
+  local setup_code_actions = require("lvim.lsp.null-ls.code_actions").setup
+  local refactorin_opts = nls.builtins.code_actions.refactoring.with {
+    filetypes = { "typescript", "javascript", "lua", "c", "cpp", "go", "python", "java", "php" },
+  }
+  setup_code_actions { refactorin_opts, nls.builtins.code_actions.gitsigns }
+  --#endregion
+
   local graphql_lsp_opts = {
     filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript" },
   }
@@ -62,11 +91,6 @@ function M.config()
   --     },
   --   },
   -- })
-  local setup_code_actions = require("lvim.lsp.null-ls.code_actions").setup
-  local refactorin_opts = nls.builtins.code_actions.refactoring.with {
-    filetypes = { "typescript", "javascript", "lua", "c", "cpp", "go", "python", "java", "php" },
-  }
-  setup_code_actions { refactorin_opts, nls.builtins.code_actions.gitsigns }
 end
 
 return M
