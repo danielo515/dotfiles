34623394700ed9a2343023e01af2ce200e1ea598
Danielo Rodriguez <rdanielo@gmail.com>
2022-10-12 07:23:22 +0200

    feat: improve dial defaults to use bool

--- chezmoi/dot_config/lvim/exact_lua/user/plugins/dial-config.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/plugins/dial-config.lua
@@ -9,5 +9,17 @@ return {
       vmap g<C-a> <Plug>(dial-increment-additional)
       vmap g<C-x> <Plug>(dial-decrement-additional)
     ]]
+    local augend = require "dial.augend"
+    require("dial.config").augends:register_group {
+      default = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.date.alias["%Y/%m/%d"],
+        augend.date.alias["%Y-%m-%d"],
+        augend.date.alias["%m/%d"],
+        augend.date.alias["%H:%M"],
+        augend.constant.alias.bool,
+      },
+    }
   end,
 }
