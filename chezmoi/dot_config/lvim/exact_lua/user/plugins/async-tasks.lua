49414d37c0587fffb69b4fa6e8a9dfd6b1329c01
Danielo Rodriguez <rdanielo@gmail.com>
2022-10-13 13:37:31 +0200

    hotfix: async tasks

--- chezmoi/dot_config/lvim/exact_lua/user/plugins/async-tasks.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/plugins/async-tasks.lua
@@ -1,13 +1,17 @@
 return {
-  "skywind3000/asynctasks.vim",
-  requires = {
-    { "skywind3000/asyncrun.vim" },
-  },
-  setup = function()
-    vim.cmd [[
+  -- I provide this dependency separately so packer stops annoying me about removing it
+  "skywind3000/asyncrun.vim",
+  {
+    "skywind3000/asynctasks.vim",
+    requires = {
+      "skywind3000/asyncrun.vim",
+    },
+    setup = function()
+      vim.cmd [[
           let g:asyncrun_open = 8
           let g:asynctask_template = '~/.config/lvim/task_template.ini'
           let g:asynctasks_extra_config = ['~/.config/lvim/tasks.ini']
-        ]]
-  end,
+          ]]
+    end,
+  },
 }
