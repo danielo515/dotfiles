b446c846c53e952d8d6e721c4a6f1b078194358f
Danielo Rodriguez <rdanielo@gmail.com>
2022-11-08 07:10:45 +0100

    chore: use danielo utils to create sibling file

--- chezmoi/dot_config/lvim/exact_lua/user/util/create.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/util/create.lua
@@ -2,8 +2,10 @@ local M = {}
 
 function M.create_sibling_file()
   local path = vim.fn.expand "%:p:h"
-  local filename = vim.fn.inputdialog("New file name> ", "")
-  vim.cmd("e " .. path .. "/" .. filename)
+  local currentFileName = vim.fn.expand "%:t:r"
+  D.vim.input(function(filename)
+    vim.cmd("e " .. path .. "/" .. filename)
+  end, { currentFileName })
 end
 
 return M
