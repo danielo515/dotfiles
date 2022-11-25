2b2141db9937becb0f9831b2dd87cb7def78f701
Danielo Rodriguez <rdanielo@gmail.com>
2022-11-25 07:12:58 +0100

    chore: neovim show source selector

--- chezmoi/dot_config/lvim/exact_lua/user/neo_tree.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/neo_tree.lua
@@ -42,6 +42,10 @@ M.plugin = {
     -- in the form "LspDiagnosticsSignWarning"
 
     require("neo-tree").setup {
+      source_selector = {
+        winbar = true,
+        statusline = true,
+      },
       close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
       popup_border_style = "rounded",
       enable_git_status = true,
