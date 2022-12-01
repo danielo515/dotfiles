0d72bd76bf9ea5ea585792d448d868a4a8112b07
Danielo Rodriguez <rdanielo@gmail.com>
2022-12-01 10:30:15 +0100

    fix: luasnip exit on insert leave

--- chezmoi/dot_config/lvim/exact_lua/user/builtin/luasnip.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/builtin/luasnip.lua
@@ -43,3 +43,17 @@ luasnip.config.set_config {
     },
   },
 }
+
+--Workaround to leave snippet mode when you leave insert mode
+--Borrowed form https://github.com/L3MON4D3/LuaSnip/issues/656#issuecomment-1313310146
+local unlinkgrp = vim.api.nvim_create_augroup("UnlinkSnippetOnModeChange", { clear = true })
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = unlinkgrp,
+  pattern = { "s:n", "i:*" },
+  desc = "Forget the current snippet when leaving the insert mode",
+  callback = function(evt)
+    if luasnip.session and luasnip.session.current_nodes[evt.buf] and not luasnip.session.jump_active then
+      luasnip.unlink_current()
+    end
+  end,
+})
