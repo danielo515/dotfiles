71c3b95c02dd7b6608e8d623f563c7123d633277
Danielo Rodriguez <rdanielo@gmail.com>
2022-12-05 07:03:10 +0100

    legendary: autoregister false

--- chezmoi/dot_config/lvim/exact_lua/user/plugins/legendary-config.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/plugins/legendary-config.lua
@@ -3,10 +3,10 @@ local function config()
   local mappings = lvim.builtin.which_key.mappings
   local wich_opts = lvim.builtin.which_key.opts
   legendary.setup {
-    auto_register_which_key = true,
     which_key = {
       mappings = mappings,
       opts = wich_opts,
+      auto_register = false,
       -- false if which-key.nvim handles binding them,
       -- set to true if you want legendary.nvim to handle binding
       -- the mappings; if not passed, true by default
