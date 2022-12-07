c5de79f08eb3bea7137e926430cf26f1fbc1289e
Danielo Rodriguez <rdanielo@gmail.com>
2022-12-07 09:59:07 +0100

    chore: leap paste auto

--- chezmoi/dot_config/lvim/exact_lua/user/plugins/leap.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/plugins/leap.lua
@@ -12,11 +12,12 @@ return {
         -- window.
         -- You can also use 'rest' & 'move' as mnemonics.
         remote = { window = "r", cross_window = "R" },
+        -- magnetic means you are going to move to the position where you are yanking
         magnetic = { window = "m", cross_window = "M" },
       },
       -- If this option is set to true, the yanked text will automatically be pasted
       -- at the cursor position if the unnamed register is in use.
-      paste_on_remote_yank = false,
+      paste_on_remote_yank = true,
     }
   end,
 }
