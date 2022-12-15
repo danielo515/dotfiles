bd63e4cf99210195f509548c1da9d19b19f0484e
Danielo Rodriguez <rdanielo@gmail.com>
2022-12-15 17:01:23 +0100

    fix: improve apigen types a bit

--- danielo_nvim/src/ApiGen.hx
+++ danielo_nvim/src/ApiGen.hx
@@ -4,22 +4,25 @@ import haxe.macro.Expr;
 import sys.io.File;
 
 var patches = [
-	"nvim_create_augroup" => macro :vim.Vim.Group,
-	"nvim_create_user_command.command" => macro :vim.VimTypes.LuaObj<vim.Vim.CommandCallbackArgs>->Void,
-	"nvim_create_user_command.opts" => macro :TableWrapper<{desc:String, force:Bool}>
+	"nvim_create_augroup" => macro:vim.Vim.Group,
+	"nvim_create_user_command.command" => macro:vim.VimTypes.LuaObj<vim.Vim.CommandCallbackArgs>->Void,
+	"nvim_create_user_command.opts" => macro:TableWrapper<{
+		desc:String,
+		force:Bool
+	}>
 ];
 
 macro function generateApi():Void {
 	var specs = Json.parse(File.getContent('res/nvim-api.json'));
 
-	Context.defineType( {
+	Context.defineType({
 		pack: ["nvim"],
 		name: "API",
 		isExtern: true,
 		kind: TDClass(),
 		meta: [meta("native", [macro "vim.api"])],
 		fields: [
-			for (f in (specs.functions:Array<FunctionDef>)) {
+			for (f in(specs.functions : Array<FunctionDef>)) {
 				{
 					name: f.name,
 					access: [AStatic, APublic],
@@ -28,7 +31,7 @@ macro function generateApi():Void {
 						args: f.parameters.map(p -> ({
 							name: p.name,
 							type: resolveType(f.name, p.name, p.type)
-						}:FunctionArg)),
+						} : FunctionArg)),
 						ret: resolveType(f.name, null, f.return_type)
 					}),
 					pos: (macro null).pos
@@ -41,23 +44,42 @@ macro function generateApi():Void {
 
 function resolveType(fun:String, arg:Null<String>, t:String):ComplexType {
 	var patch = patches.get(arg == null ? fun : '$fun.$arg');
-	if (patch != null) return patch;
+	if (patch != null)
+		return patch;
 
 	return switch (t) {
-		case "String": macro :String;
-		case "Integer": macro :Int;
-		case "Boolean": macro :Bool;
-		case "Object": macro :Dynamic;
-		case "Array": macro :Array<Dynamic>;
-		case "void": macro :Void;
+		case "String": macro
+		:String;
+		case "Window": macro
+		:vim.VimTypes.WindowId;
+		case "Buffer": macro
+		:vim.VimTypes.BufferId;
+		case "Integer": macro
+		:Int;
+		case "Float": macro
+		:Float;
+		case "Dictionary": macro
+		:lua.Table<String, Dynamic>;
+		case "Boolean": macro
+		:Bool;
+		case "Object": macro
+		:Dynamic;
+		case "Array": macro
+		:Array<Dynamic>;
+		case "void": macro
+		:Void;
 
 		case t if (StringTools.startsWith(t, "ArrayOf(")):
-			var itemType = resolveType(fun, (arg == null ? "" : arg) + "[]", t.split("(")[1].split(",")[0]);
-			macro :Array<$itemType>;
+			final regexArrayArg = ~/ArrayOf\(([a-zA-Z]+),?/i;
+			regexArrayArg.match(t);
+			var itemType = resolveType(fun, (arg == null ? "" : arg) + "[]", regexArrayArg.matched(1));
+			macro
+		:Array<$itemType>;
 
 		case _:
 			Context.warning('Cannot resolve type $t', (macro null).pos);
-			macro :Dynamic;
+			macro
+		:Dynamic;
 	};
 }
 
@@ -76,8 +98,12 @@ typedef FunctionDef = {
 
 abstract ParamDef(Array<String>) {
 	public var type(get, never):String;
-	function get_type():String return this[0];
+
+	function get_type():String
+		return this[0];
 
 	public var name(get, never):String;
-	function get_name():String return this[1];
+
+	function get_name():String
+		return this[1];
 }
--- danielo_nvim/src/Main.hx
+++ danielo_nvim/src/Main.hx
@@ -6,7 +6,6 @@ class Main {
 	static function main() {
 		// vim.Ui.select(["a"], {prompt: "Pick one sexy option"}, (choice, _) -> Vim.print(choice));
 		vim.Api.nvim_create_user_command("HaxeCmd", (args) -> Vim.print(args), {desc: "Testing from haxe", force: true});
-		nvim.API.nvim_create_user_command("HaxeCmd", (args) -> Vim.print(args), {desc: "Testing from haxe", force: true});
 
 		DanieloVim.autocmd('HaxeEvent', [BufWritePost], "*.hx", "Created from haxe", () -> {
 			var filename = Vim.expand(ExpandString.plus(CurentFile, FullPath));
