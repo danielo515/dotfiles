c59349a082d5e5f04498047d1b2bbe63e643a70d
Danielo Rodriguez <rdanielo@gmail.com>
2023-01-08 13:23:01 +0100

    fix: colorcolumn

--- danielo_nvim/lua/danielo_nvim/kickstart.lua
+++ danielo_nvim/lua/danielo_nvim/kickstart.lua
@@ -815,7 +815,6 @@ __kickstart__Kickstart_Kickstart_Fields_.main = function()
   vim.o.breakindent = true;
   vim.o.undofile = true;
   vim.wo.number = true;
-  vim.wo.colorcolumn = "yes";
   __kickstart__Kickstart_Kickstart_Fields_.keymaps();
   __kickstart_Lualine.setup(({options = ({icons_enabled = true, theme = "onedark", component_separators = "|", section_separators = ""})}));
   __kickstart_Comment.setup();
