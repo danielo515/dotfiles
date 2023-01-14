f57a011eb09747dc2f69f109aebe7dc66c5b0084
Danielo Rodriguez <rdanielo@gmail.com>
2023-01-14 05:58:21 +0100

    chore: do not use ufo for now

--- chezmoi/dot_config/lvim/exact_lua/user/plugins/ufo-config.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/plugins/ufo-config.lua
@@ -7,9 +7,10 @@ return {
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
 
-    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
     vim.keymap.set("n", "zR", require("ufo").openAllFolds)
     vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+    vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+    vim.keymap.set("n", "zm", require("ufo").closeFoldsWith)
     -- Option 2: nvim lsp as LSP client
     -- Tell the server the capability of foldingRange,
     -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
