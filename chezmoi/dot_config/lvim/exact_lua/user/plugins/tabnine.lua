04a8b0c343050d902c3f0416ea5b9ab9b88e1ed5
Danielo Rodriguez <rdanielo@gmail.com>
2023-01-20 12:06:35 +0100

    chore: cmp tabnine

--- chezmoi/dot_config/lvim/exact_lua/user/plugins/tabnine.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/plugins/tabnine.lua
@@ -1,12 +1,13 @@
 return {
   "codota/tabnine-nvim",
+  requires = { "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp" },
   config = function()
     require("tabnine").setup {
       disable_auto_comment = true,
-      accept_keymap = "<Tab>",
+      accept_keymap = "<S-tab>",
       dismiss_keymap = "<C-e>",
       debounce_ms = 300,
-      suggestion_color = { gui = "#808080", cterm = 244 },
+      suggestion_color = { gui = "#bb9af7", cterm = 244 },
       execlude_filetypes = { "TelescopePrompt" },
     }
   end,
