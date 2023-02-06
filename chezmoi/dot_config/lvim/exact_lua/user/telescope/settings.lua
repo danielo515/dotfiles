61ee08f940082b06eb06961c49dd4002d41cf707
Danielo Rodriguez <rdanielo@gmail.com>
2023-02-06 08:06:37 +0100

    telescope default previewer

--- chezmoi/dot_config/lvim/exact_lua/user/telescope/settings.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/telescope/settings.lua
@@ -13,6 +13,27 @@ local plugins = {
   "nvim-telescope/telescope-symbols.nvim",
 }
 
+local previewers = require "telescope.previewers"
+
+-- Little wrapper around default telescope previewer
+-- that prevents to preview files that are larger than certain
+-- size.
+local new_maker = function(filepath, bufnr, opts)
+  opts = opts or {}
+
+  filepath = vim.fn.expand(filepath)
+  vim.loop.fs_stat(filepath, function(_, stat)
+    if not stat then
+      return
+    end
+    if stat.size > 10000 then
+      return
+    else
+      previewers.buffer_previewer_maker(filepath, bufnr, opts)
+    end
+  end)
+end
+
 lvim.builtin.telescope.path_display = "truncate"
 
 lvim.builtin.telescope.on_config_done = function(tele)
@@ -60,6 +81,9 @@ lvim.builtin.telescope.on_config_done = function(tele)
     },
   }
   local opts = {
+    defaults = {
+      buffer_previewer_maker = new_maker,
+    },
     -- Default mappings are handled below
     pickers = {
       highlights = {
