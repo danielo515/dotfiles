4a514b14b45dd1344f99eee4e6f12acfc558e091
Danielo Rodriguez <rdanielo@gmail.com>
2023-02-14 06:27:46 +0100

    chore: hope this triggers a release

--- chezmoi/dot_config/lvim/exact_lua/user/plugins/copilot-config.lua
+++ chezmoi/dot_config/lvim/exact_lua/user/plugins/copilot-config.lua
@@ -6,7 +6,7 @@ return {
     require("copilot").setup {
       panel = {
         enabled = true,
-        auto_refresh = false,
+        auto_refresh = true,
         keymap = {
           jump_prev = "[[",
           jump_next = "]]",
@@ -21,7 +21,7 @@ return {
       },
       suggestion = {
         enabled = true,
-        auto_trigger = false,
+        auto_trigger = true,
         debounce = 75,
         keymap = {
           accept = "<S-Tab>",
