0fc6c900cf5b2c0112ff5d0ce57df35e87557f1c
Danielo Rodriguez <rdanielo@gmail.com>
2023-02-27 09:30:12 +0100

    chore: all plugins migrated to Pure

--- kickstart.hx/src/kickstart/Pure.hx
+++ kickstart.hx/src/kickstart/Pure.hx
@@ -20,7 +20,27 @@ function main() {
         // Additional lua configuration, makes nvim stuff amazing
         "folke/neodev.nvim",
       ]),
-    }
+    },
+    { // Autocompletion
+      name: "hrsh7th/nvim-cmp",
+      requires: t(["hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip"]),
+    },
+    { // Highlight, edit, and navigate code
+      name: "nvim-treesitter/nvim-treesitter",
+      run: 'pcall(require("nvim-treesitter.install").update({ with_sync = true }))'
+    },
+
+    {name: "b0o/schemastore.nvim"},
+    // Git related plugins
+    {name: "tpope/vim-fugitive"},
+    {name: "tpope/vim-rhubarb"},
+    {name: "lewis6991/gitsigns.nvim"},
+
+    {name: "navarasu/onedark.nvim"}, // Theme inspired by Atom
+    {name: "nvim-lualine/lualine.nvim"}, // Fancier statusline
+    {name: "lukas-reineke/indent-blankline.nvim"}, // Add indentation guides even on blank lines
+    {name: "numToStr/Comment.nvim"}, // "gc" to comment visual regions/lines
+    {name: "tpope/vim-sleuth"}, // Detect tabstop and shiftwidth automatically
   ];
   Packer.init(plugins);
 }
