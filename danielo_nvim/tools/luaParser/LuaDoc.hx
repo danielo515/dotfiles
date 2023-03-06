34b2c8b18350f17d2d6868e09facb3f0b0ac6f39
Danielo Rodriguez <rdanielo@gmail.com>
2023-03-06 06:33:18 +0100

    chore: parsing

--- danielo_nvim/tools/luaParser/LuaDoc.hx
+++ danielo_nvim/tools/luaParser/LuaDoc.hx
@@ -10,11 +10,20 @@ typedef Param = {
   final description:String;
 }
 
+enum DocType {
+  Function;
+  Number;
+  String;
+  Table;
+  Boolean;
+}
+
 enum DocToken {
   Identifier(name:String);
+  Description(text:String);
   ArrayMod;
   OptionalMod;
-  Keyword(name:String);
+  DocType(type:DocType);
   Comma;
   CurlyOpen;
   CurlyClose;
@@ -24,27 +33,64 @@ enum DocToken {
   Rparen;
   TypeOpen;
   TypeClose;
+  Pipe;
   EOL;
 }
 
 class LuaDocLexer extends Lexer implements hxparse.RuleBuilder {
+  public static var desc = @:rule ["[^\n]*" => Description(lexer.current),];
   public static var paramDoc = @:rule [
     "[a-zA-z]+" => {
       final name = lexer.current;
       Identifier(name);
     },
-    " " => lexer.token(paramDoc),
-    "," => lexer.token(paramDoc),
+    " +" => lexer.token(paramDoc),
+  ];
+  public static var typeDoc = @:rule [
+    " " => lexer.token(typeDoc),
+    "," => lexer.token(typeDoc),
     "\\[\\]" => ArrayMod,
     "\\?" => OptionalMod,
     "<" => TypeOpen,
     ">" => TypeClose,
-    "{" => CurlyClose,
+    "{" => CurlyOpen,
     "}" => CurlyClose,
     "[" => SquareOpen,
     "]" => SquareClose,
     "\\(" => Lparen,
     "\\)" => Rparen,
+    "\\|" => Pipe,
     "" => null,
   ];
+}
+
+class LuaDocParser extends hxparse.Parser< hxparse.LexerTokenSource< DocToken >, DocToken > implements hxparse.ParserBuilder {
+  public function new(input:byte.ByteData) {
+    var lexer = new LuaDocLexer(input);
+    var ts = new hxparse.LexerTokenSource(lexer, LuaDocLexer.paramDoc);
+    super(ts);
+  }
+
+  public function parse() {
+    return switch stream {
+      case [
+        Identifier(name),
+        t = parseType(),
+        Description(text),
+        EOL
+      ]:
+        return [name, t, text];
+    }
+  }
+
+  public function parseType() {
+    return switch stream {
+      case [CurlyOpen, t = parseType(), CurlyClose,]:
+        t + "";
+      case [DocType(t), Pipe, t2 = parseType()]:
+        'Either($t, $t2)';
+      case [DocType(t)]:
+        t + "";
+    }
+  }
 }
