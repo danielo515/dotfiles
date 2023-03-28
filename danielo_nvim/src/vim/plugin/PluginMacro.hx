57cfc2ac517d133a490c80baffcb255adc37d0ed
Danielo Rodriguez <rdanielo@gmail.com>
2023-03-28 08:34:23 +0200

    WIP: make module helper

--- danielo_nvim/src/vim/plugin/PluginMacro.hx
+++ danielo_nvim/src/vim/plugin/PluginMacro.hx
@@ -1,11 +1,35 @@
 package vim.plugin;
 
 using haxe.macro.TypeTools;
+using haxe.macro.ExprTools;
 
 import haxe.macro.Context;
 import haxe.macro.Expr;
 
 class PluginMacro {
+  /**
+    helper to transform the values passed to the @module annotation.
+    It expects the **contents** of the annotation which should look like this:
+    @module(["name" => Type, "name2" => Type2])
+    e.g.:
+    ```haxe 
+      case {meta: [{name: "module", params: [p]}]}: makeModule(p.expr);
+    ```
+  **/
+  static public function makeModule(e:ExprDef) {
+    return switch e {
+      case EArrayDecl(f):
+        f.map(x -> switch x {
+          case macro $lh => $rh:
+            {name: lh.getValue(), type: rh.toString()};
+          case _: throw "Not what I expected";
+        });
+      case x:
+        trace("sorry but", x);
+        throw "unexpected construct";
+    }
+  }
+
   macro static public function pluginInterface():Array< Field > {
     final fields = Context.getBuildFields();
     final localType = Context.getLocalType().toComplexType();
