86f09ebecb5c2c952cc0d25a97b73c5cc6ea8395
Danielo Rodriguez <rdanielo@gmail.com>
2023-04-04 13:30:30 +0200

    chore: hamerspoon auto flasher

--- chezmoi/dot_hammerspoon/init.lua
+++ chezmoi/dot_hammerspoon/init.lua
@@ -88,4 +88,41 @@ end)
 hs.hotkey.bind(hyper, "w", osa("whatsapp"))
 hs.hotkey.bind(hyper, "i", osa("inbox"))
 
+-- Automatically copy firmwares to the keyboard
+local sourceFilePath = "/Users/danielo/GIT/glove80-zmk-config/combined/glove80.uf2"
+local destinationVolumePath = "/Volumes/GLV80LHBOOT"
+
+function cpyGlove80uf2(data)
+	if data.eventType == "added" and data.productName == "Glove80 LH Bootloader" then
+		local fileExists = hs.fs.attributes(sourceFilePath, "mode")
+		if fileExists == nil then
+			hs.notify.new({ title = "File not found", informativeText = "The file to be copied was not found." }):send()
+		else
+			hs.timer.doAfter(2, function()
+				local cmd = "cp " .. sourceFilePath .. " " .. destinationVolumePath .. "/"
+				local stdout, status, signal, result = hs.execute(cmd)
+				if result == 0 then
+					hs.notify
+						.new({
+							title = "File copied",
+							informativeText = "The file was successfully copied to the external drive.",
+						})
+						:send()
+				else
+					hs.notify
+						.new({
+							title = "Copy failed",
+							informativeText = "There was an error copying the file to the external drive.",
+						})
+						:send()
+					hs.printf("Error message: %s", stdout)
+				end
+			end)
+		end
+	end
+end
+
+usbWatcher = hs.usb.watcher.new(cpyGlove80uf2)
+usbWatcher:start()
+
 hs.alert.show("Config loaded", { text = "red" })
