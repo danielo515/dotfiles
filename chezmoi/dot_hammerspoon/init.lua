bfd286310d5c279cf68c6547da2b4cb90bd788be
Danielo Rodriguez <rdanielo@gmail.com>
2023-04-20 10:34:14 +0200

    hs: hack the alert styles

--- chezmoi/dot_hammerspoon/init.lua
+++ chezmoi/dot_hammerspoon/init.lua
@@ -65,4 +65,26 @@ Hyper:bind({}, "j", function()
 	App.launchOrFocusByBundleID("net.kovidgoyal.kitty")
 end)
 
-hs.alert.show("Config loaded", { text = "red" })
+local alertStyle = {
+	atScreenEdge = 0,
+	fadeInDuration = 0.15,
+	fadeOutDuration = 0.15,
+	fillColor = {
+		alpha = 0.75,
+		white = 0,
+	},
+	radius = 17,
+	strokeColor = {
+		alpha = 1,
+		white = 1,
+	},
+	strokeWidth = 2,
+	textColor = {
+		alpha = 1,
+		red = 1,
+	},
+	textFont = ".AppleSystemUIFont",
+	textSize = 27,
+}
+
+hs.alert.show("Config loaded", alertStyle)
