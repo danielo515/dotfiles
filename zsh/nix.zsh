23796b307224d3edda1bf307c7317376c2efc8ed
Danielo Rodriguez <rdanielo@gmail.com>
2023-04-23 13:22:48 +0200

    chezmoi: update templates

--- zsh/nix.zsh
+++ zsh/nix.zsh
@@ -1,6 +1,3 @@
-
-# Nix
 if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 fi
-# End Nix
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -134,18 +134,6 @@ export PATH="$PATH:$DOTFILES/bin"
 if [[ -f "$HOME/.xsh" ]]; then
     source $HOME/.xsh
 fi
-
-# -- fzf -----------------------------------------------------------------------
-if [ -f ~/.fzf.zsh ]; then
-    source ~/.fzf.zsh
-    export FZF_TMUX=1
-    export FZF_DEFAULT_OPTS="--color 16,info:6,hl:13,hl+:13"
-    export FZF_DEFAULT_COMMAND="rg --files-with-matches --no-messages -F ''"
-    export FZF_CTRL_T_COMMAND="rg --files-with-matches --no-messages --hidden -F '' | grep -v .git/"
-    # fzf-tab opens in a nice popup
-    zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
-fi
-
 # -- asdf ---------------------------------------------------------------------
 if [[ -d "$HOME/.asdf" ]]; then
     source $HOME/.asdf/asdf.sh
