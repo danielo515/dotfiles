9544447fcb33f0aac3e1cdf0b64bb90e14714ce8
Danielo Rodriguez <rdanielo@gmail.com>
2023-05-11 19:10:58 +0200

    feat: images to html uses grid

--- bin/images_to_html.sh
+++ bin/images_to_html.sh
@@ -3,44 +3,72 @@
 output_path=${1:-"$(pwd)/images.html"}
 
 # Set the default filter to reject hidden files and directories
-default_filter="-not -path '*/.*'"
+default_filter="-H"
 
 # Combine the default filter with the user filter, if provided
 if [ -n "$2" ]; then
-  filter="-not -path '$2'"
+  filter="$2"
 else
   filter="$default_filter"
 fi
 
 # Create an array of all image files in the current directory and its subdirectories
-images=( $(find . -type f -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" $filter) )
+images=( $(fd -e jpg -e jpeg -e png -e gif -e svg $filter) )
 
-# Start the HTML file
-echo "<html><body><table>" > "$output_path"
+# Set the number of columns (default: 4)
+columns=${3:-4}
 
-# Set the number of images to display per row
-images_per_row=4
+# Declare HTML sections as separate strings enclosed within parentheses
+html_start=$(cat << HTML
+<html>
+<head>
+<style>
+:root {
+  --column-count: $columns;
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(50px, 1fr));
+  grid-gap: 10px;
+}
+.image-container img {
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+}
+</style>
+</head>
+<body>
+HTML
+)
 
-# Loop through the images array and add each image to the HTML file
+html_number_input=$(cat << HTML
+<label for="column-input">Number of Columns:</label>
+<input type="number" id="column-input" min="1" max="10" value="$columns"
+  oninput="document.documentElement.style.setProperty('--column-count', this.value);">
+HTML
+)
+
+html_grid_start="<div class=\"grid-container\">"
+html_grid_end="</div>"
+html_end="</body></html>"
+
+# Concatenate the HTML content
+html_content="${html_start}${html_number_input}${html_grid_start}"
+
+# Loop through the images array and add each image to the HTML content
 echo -n "Processing images"
-for (( i=0; i<${#images[@]}; i+=images_per_row )); do
+for image in "${images[@]}"; do
     echo -n "."
-    echo "<tr>" >> "$output_path"
-    for (( j=0; j<$images_per_row; j++ )); do
-        # Get the filename and absolute path for the current image
-        index=$((i+j))
-        if [[ $index -lt ${#images[@]} ]]; then
-            image="${images[$index]}"
-            filename=$(basename "$image")
-            path=$(cd "$(dirname "$image")"; pwd)
+    filename=$(basename "$image")
+    path=$(dirname "$image")
 
-            # Add the image to the HTML file
-            echo "<td><img src=\"$path/$filename\"><br>$filename<br>$path</td>" >> "$output_path"
-        fi
-    done
-    echo "</tr>" >> "$output_path"
+    # Add the image to the HTML content with a fixed size container
+    html_content+="<div class=\"image-container\"><img src=\"$image\"><br>$filename<br>$path</div>"
 done
 echo " done."
 
-# End the HTML file
-echo "</table></body></html>" >> "$output_path"
+html_content+="${html_grid_end}${html_end}"
+
+# Write the HTML content to the file
+echo "$html_content" > "$output_path"
