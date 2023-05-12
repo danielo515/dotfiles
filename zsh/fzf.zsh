bcc50c1f6de4d973c0b6d222b1376b7cf57215cb
Danielo Rodriguez <rdanielo@gmail.com>
2023-05-12 10:55:32 +0200

    fzf: better defaults

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,9 +1,9 @@
 if [ -f ~/.fzf.zsh ]; then
     source ~/.fzf.zsh
     export FZF_TMUX=1
-    export FZF_DEFAULT_OPTS="--color 16,info:6,hl:13,hl+:13"
+    export FZF_DEFAULT_OPTS="--color 16,info:6,hl:13,hl+:13 --height=80% --layout=reverse"
     export FZF_DEFAULT_COMMAND="rg --files-with-matches --no-messages -F ''"
-    export FZF_CTRL_T_COMMAND="rg --files-with-matches --no-messages --hidden -F '' | grep -v .git/"
+    export FZF_CTRL_T_COMMAND="fd | grep -v .git/"
     # fzf-tab opens in a nice popup
     zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
 fi
