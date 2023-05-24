b2c8a55fe6d1d0b58ccc30a3af45c87771b9f20c
Danielo Rodriguez <rdanielo@gmail.com>
2023-05-24 09:21:53 +0200

    hs: state machine moves before returning

--- chezmoi/dot_hammerspoon/lib/stateMachine.lua
+++ chezmoi/dot_hammerspoon/lib/stateMachine.lua
@@ -1,35 +1,33 @@
 -- Define the createStateMachine function
 local function createStateMachine(states)
-	-- Define the initial state
-	local currentStateIndex = 1
-	-- Define the state machine function
-	local function stateMachine()
-		-- Get the current state
-		local currentState = states[currentStateIndex]
-		-- Move to the next state
-		currentStateIndex = currentStateIndex + 1
+  -- Define the initial state
+  local currentStateIndex = 1
+  -- Moves the state forward and returns that state
+  local function stateMachine()
+    -- Move to the next state
+    currentStateIndex = currentStateIndex + 1
 
-		-- If we've reached the end of the list, start from the beginning
-		if currentStateIndex > #states then
-			currentStateIndex = 1
-		end
-		return currentState
-	end
+    -- If we've reached the end of the list, start from the beginning
+    if currentStateIndex > #states then
+      currentStateIndex = 1
+    end
+    return states[currentStateIndex]
+  end
 
-	-- Define the reset method
-	local function reset()
-		currentStateIndex = 1
-	end
+  -- Define the reset method
+  local function reset()
+    currentStateIndex = 1
+  end
 
-	local function getCurrentState()
-		return states[currentStateIndex]
-	end
+  local function getCurrentState()
+    return states[currentStateIndex]
+  end
 
-	-- Return a callable table with both the stateMachine function and the reset method
-	return setmetatable({}, {
-		__call = stateMachine,
-		__index = { reset = reset, getCurrentState = getCurrentState },
-	})
+  -- Return a callable table with both the stateMachine function and the reset method
+  return setmetatable({}, {
+    __call = stateMachine,
+    __index = { reset = reset, getCurrentState = getCurrentState },
+  })
 end
 
 return createStateMachine
