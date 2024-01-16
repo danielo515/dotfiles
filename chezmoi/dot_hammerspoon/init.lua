474b9becfebb8c21c1d3ed2f40f43617096990f0
Danielo Rodriguez <rdanielo@gmail.com>
2024-01-16 11:42:58 +0100

    hammerspoon: refactor keys and deactivate vercel

--- chezmoi/dot_hammerspoon/init.lua
+++ chezmoi/dot_hammerspoon/init.lua
@@ -3,44 +3,36 @@ hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 StreamDeck = hs.loadSpoon("StreamDeckButton")
 StreamDeck:start()
-StreamDeck:onWillAppear("vercelStatus", function(context, params)
-  print("vercelStatus willAppear", context)
-  return StreamDeck:getImageMessage(
-    context,
-    -- "~/Pictures/danielo515_programmer_fighting_blasphemy_5303f07b-4184-87fb-f043d10e18c2.png"
-    "~/Pictures/tv-test.png"
-  )
-end)
 hs.loadSpoon("EmmyLua")
 hs.grid.setGrid("10x6")
 local stateMachine = require("lib.stateMachine")
 local slack = require("lib.slack")
 
 local function isMonitorBig()
-  local windowWidth = hs.screen.mainScreen():frame().w;
-  if windowWidth < 2000 then
-    return false
-  end
+	local windowWidth = hs.screen.mainScreen():frame().w
+	if windowWidth < 2000 then
+		return false
+	end
 end
 
 WorkStates = stateMachine({
-  { name = "Morning", icon = "~/.config/icons/cool.svg" },
-  { name = "Workout", icon = "~/.config/icons/workout.svg" },
-  { name = "Lunch",   icon = "~/.config/icons/burguer.svg" },
+	{ name = "Morning", icon = "~/.config/icons/cool.svg" },
+	{ name = "Workout", icon = "~/.config/icons/workout.svg" },
+	{ name = "Lunch", icon = "~/.config/icons/burguer.svg" },
 })
 
 StreamDeck:onWillAppear("routine", function(context, params)
-  local state = WorkStates.getCurrentState()
-  local message = StreamDeck:getImageMessage(context, state.icon)
-  StreamDeck:setTitle("routine", state.name)
-  return message
+	local state = WorkStates.getCurrentState()
+	local message = StreamDeck:getImageMessage(context, state.icon)
+	StreamDeck:setTitle("routine", state.name)
+	return message
 end)
 
 StreamDeck:onKeyDown("routine", function(context, params)
-  local state = WorkStates()
-  StreamDeck:setTitle("routine", state.name)
-  slack.gotoChat();
-  return StreamDeck:getImageMessage(context, state.icon)
+	local state = WorkStates()
+	StreamDeck:setTitle("routine", state.name)
+	slack.gotoChat()
+	return StreamDeck:getImageMessage(context, state.icon)
 end)
 
 local secrets = require("secrets")
@@ -55,68 +47,78 @@ local featherWindowTitle = "Feather.*"
 local chrome_app_name = "Google Chrome"
 local wf = hs.window.filter
 local positions = require("windowing").positions
+Chrome = require("browser")("Google Chrome")
+Danielo = { timer = nil }
+--[[
+StreamDeck:onWillAppear("vercelStatus", function(context, params)
+	print("vercelStatus willAppear", context)
+	return StreamDeck:getImageMessage(
+		context,
+		-- "~/Pictures/danielo515_programmer_fighting_blasphemy_5303f07b-4184-87fb-f043d10e18c2.png"
+		"~/Pictures/tv-test.png"
+	)
+end)
 WatchVercel = require("watch_vercel")
 StreamDeck:onKeyDown("vercelStatus", function()
   print("vercelStatus keyDown")
   WatchVercel.openLatest()
 end)
-Chrome = require("browser")("Google Chrome")
-Danielo = { timer = nil }
 local vercel = hs.settings.get("secrets").tella.vercel
 WatchVercel.start(function(status)
   StreamDeck:setTitle("vercelStatus", status)
   statusbar:setTitle("🚦" .. status)
 end, vercel.teamId, vercel.token)
+]]
 
 -- Windows
 local function locateFeather(window)
-  local windowLayout = {
-    { nil, window, retina, hs.layout.left50, nil, nil },
-  }
-  hs.layout.apply(windowLayout)
+	local windowLayout = {
+		{ nil, window, retina, hs.layout.left50, nil, nil },
+	}
+	hs.layout.apply(windowLayout)
 end
 
 local function subscribeToFocus(appName, callback, filterOptions)
-  local options = filterOptions or {}
-  wf.new(false):setAppFilter(appName, options):subscribe(hs.window.filter.windowFocused, callback, true)
+	local options = filterOptions or {}
+	wf.new(false):setAppFilter(appName, options):subscribe(hs.window.filter.windowFocused, callback, true)
 end
 
 subscribeToFocus(chrome_app_name, locateFeather, { allowTitles = featherWindowTitle })
 -- Slack listener
 subscribeToFocus("Slack", function(window)
-  if not isMonitorBig() then
-    return
-  end
-  local layout = {
-    { nil, window, primaryScreen, hs.geometry.rect(0.3, 0, 0.45, 0.95), nil, nil },
-  }
-  hs.layout.apply(layout)
+	if not isMonitorBig() then
+		return
+	end
+	local layout = {
+		{ nil, window, primaryScreen, hs.geometry.rect(0.3, 0, 0.45, 0.95), nil, nil },
+	}
+	hs.layout.apply(layout)
 end, { rejectTitles = "Huddle" })
 
--- Whenever we focus kitty, we position chrome to the right so we can see references
+-- Whenever we focus the terminal of choice, we position chrome to the right so we can see references
 -- or the web app we are working with
 subscribeToFocus("Alacritty", function(window)
-  local function notHuddle(name, title)
-    return not name:match("Huddle")
-  end
+	local function notHuddle(name, title)
+		return not name:match("Huddle")
+	end
 
-  local windowWidth = hs.window.focusedWindow():frame().w
-  if windowWidth < 2000 then
-    return
-  end
+	local windowWidth = hs.window.focusedWindow():frame().w
+	if windowWidth < 2000 then
+		return
+	end
 
-  local layout = {
-    { nil,             window,   primaryScreen, hs.layout.right70, nil, nil },
-    { chrome_app_name, nil,      primaryScreen, positions.left34,  nil, nil },
-    { "time tracker",  nil,      retina,        positions.right30, nil, nil },
-    { "Slack",         "Huddle", retina,        positions.left70,  nil, nil },
-  }
-  -- slack.gotoChat();
-  hs.layout.apply(layout, notHuddle)
+	local layout = {
+		{ nil, window, primaryScreen, hs.layout.right70, nil, nil },
+		{ chrome_app_name, nil, primaryScreen, positions.left34, nil, nil },
+		{ "time tracker", nil, retina, positions.right30, nil, nil },
+		{ "Slack", "Huddle", retina, positions.left70, nil, nil },
+	}
+	-- slack.gotoChat();
+	hs.layout.apply(layout, notHuddle)
 end)
 
 require("keybinds")
-require("auto_tile")
+-- require("auto_tile")
 require("auto_flasher")
 require("watch_files")
 
@@ -128,7 +130,7 @@ Hyper = spoon.Hyper
 Hyper:bindHotKeys({ hyperKey = { {}, "F1" } })
 
 Hyper:bind({}, "j", function()
-  App.launchOrFocusByBundleID("net.kovidgoyal.kitty")
+	App.launchOrFocusByBundleID("net.kovidgoyal.kitty")
 end)
 
 require("alert").important("Hammerspoon config loaded")
--- chezmoi/dot_hammerspoon/keybinds.lua
+++ chezmoi/dot_hammerspoon/keybinds.lua
@@ -2,120 +2,120 @@ local chrome_app_name = "Google Chrome"
 local ws = require("windowing")
 
 local function focusCenterChrome()
-  local opened = hs.application.launchOrFocus(chrome_app_name)
-  if opened then
-    hs.window.find(chrome_app_name):centerOnScreen()
-  end
+	local opened = hs.application.launchOrFocus(chrome_app_name)
+	if opened then
+		hs.window.find(chrome_app_name):centerOnScreen()
+	end
 end
 
 -- Get the currently focused application and put all its windows in a grid
 local function mainAppToGrid()
-  local mainApp = hs.application.frontmostApplication()
-  local mainAppName = mainApp:title()
-  ws.arrangeAppWindowsInGrid(mainAppName)
+	local mainApp = hs.application.frontmostApplication()
+	local mainAppName = mainApp:title()
+	ws.arrangeAppWindowsInGrid(mainAppName)
 end
 
 -- Puts the 3 most recen windows in a nice grid
 local function gridRecents()
-  hs.grid.setGrid("10x10")
-  local windows = hs.window.filter.new():getWindows()
-  local screen = hs.screen.mainScreen()
-  hs.grid.set(windows[1], { x = 5, y = 0, w = 5, h = 10 }, screen)
-  hs.grid.set(windows[2], { x = 0, y = 0, w = 5, h = 5 }, screen)
-  hs.grid.set(windows[3], { x = 0, y = 5, w = 5, h = 5 }, screen)
+	hs.grid.setGrid("10x10")
+	local windows = hs.window.filter.new():getWindows()
+	local screen = hs.screen.mainScreen()
+	hs.grid.set(windows[1], { x = 5, y = 0, w = 5, h = 10 }, screen)
+	hs.grid.set(windows[2], { x = 0, y = 0, w = 5, h = 5 }, screen)
+	hs.grid.set(windows[3], { x = 0, y = 5, w = 5, h = 5 }, screen)
 end
 
 -- toggle the hammerspoon console, focusing on the previous app when hidden
 local lastApp = nil
 local function toggleConsole()
-  local frontmost = hs.application.frontmostApplication()
-  hs.toggleConsole()
-  if frontmost:bundleID() == "org.hammerspoon.Hammerspoon" then
-    if lastApp ~= nil then
-      lastApp:activate()
-      lastApp = nil
-    end
-  else
-    lastApp = frontmost
-  end
+	local frontmost = hs.application.frontmostApplication()
+	hs.toggleConsole()
+	if frontmost:bundleID() == "org.hammerspoon.Hammerspoon" then
+		if lastApp ~= nil then
+			lastApp:activate()
+			lastApp = nil
+		end
+	else
+		lastApp = frontmost
+	end
 end
 
 -- Edit the current picture in the clipboard
 local function editClipboardImage()
-  -- Check if an image is in the clipboard
-  local image = hs.pasteboard.readImage()
-  if not image then
-    hs.alert.show("No image in clipboard")
-    return
-  end
+	-- Check if an image is in the clipboard
+	local image = hs.pasteboard.readImage()
+	if not image then
+		hs.alert.show("No image in clipboard")
+		return
+	end
 
-  -- Save the image to a temporary file
-  local tmpfile = os.tmpname() .. ".png"
-  image:saveToFile(tmpfile)
+	-- Save the image to a temporary file
+	local tmpfile = os.tmpname() .. ".png"
+	image:saveToFile(tmpfile)
 
-  -- Open the image in Preview and start annotation
-  hs.execute("open -a Preview " .. tmpfile)
-  hs.timer.doAfter(1, function()
-    hs.appfinder.appFromName("Preview"):selectMenuItem({ "Tools", "Annotate", "Arrow" })
-  end)
+	-- Open the image in Preview and start annotation
+	hs.execute("open -a Preview " .. tmpfile)
+	hs.timer.doAfter(1, function()
+		hs.appfinder.appFromName("Preview"):selectMenuItem({ "Tools", "Annotate", "Arrow" })
+	end)
 end
 
 local US_LAYOUT = "com.apple.keylayout.US"
 local hyperApps = {
-  { key = "1", appName = "Slack",                  layout = US_LAYOUT },
-  { key = "2", appName = { "Alacritty", "Kitty" }, layout = US_LAYOUT },
-  { key = "3", appName = "Google Chrome" },
-  { key = "4", appName = "Arc" },
-  { key = "o", appName = "Obsidian" },
-  {
-    key = "w",
-    callback = function()
-      Chrome.jump("whatsapp")
-    end,
-  },
-  { key = "space", callback = ws.referenceChooser },
-  { key = "v",     callback = ws.window_menu },
-  { key = "g",     callback = mainAppToGrid },
-  { key = "c",     callback = toggleConsole },
-  { key = "i",     callback = ws.debugWindow },
-  { key = "f",     callback = gridRecents },
-  { key = "p",     callback = editClipboardImage },
+	{ key = "s", appName = "Slack", layout = US_LAYOUT },
+	{ key = "2", appName = { "Alacritty", "Kitty" }, layout = US_LAYOUT },
+	{ key = "3", appName = "Google Chrome" },
+	{ key = "4", appName = "Firefox" },
+	{ key = "o", appName = "Obsidian" },
+	{
+		key = "w",
+		callback = function()
+			Chrome.jump("whatsapp")
+		end,
+	},
+	{ key = "space", callback = ws.referenceChooser },
+	{ key = "v", callback = ws.window_menu },
+	{ key = "g", callback = mainAppToGrid },
+	{ key = "c", callback = toggleConsole },
+	{ key = "i", callback = ws.debugWindow },
+	{ key = "f", callback = gridRecents },
+	{ key = "p", callback = editClipboardImage },
 }
 
 local hyper = { "cmd", "shift", "alt", "ctrl" }
 
 hs.fnutils.each(hyperApps, function(item)
-  if item.callback ~= nil then
-    hs.hotkey.bind(hyper, item.key, item.callback)
-    return
-  end
-  hs.hotkey.bind(hyper, item.key, function()
-    -- if appName is a list, then first look for opened windows of the provided list
-    -- and focus the first we find. If not, then fall back to activate the first app in the list
-    local appName = item.appName -- shortcut and also re-used below for the table fallback
-    if type(item.appName) ~= "table" then
-      appName = { item.appName }
-    end
-    for _, name in ipairs(appName) do
-      -- We prefer to use an already existing window in the current space if possible
-      local appWindows = hs.window.filter.new(false):setAppFilter(name, { currentSpace = true }):getWindows()
-      if #appWindows > 0 then
-        print("found app window in current space", name)
-        appWindows[1]:focus()
-        return
-      end
-      local app = hs.appfinder.appFromName(name)
-      if app then
-        print("found app open", name)
-        app:activate()
-        app:unhide()
-        if item.layout ~= nil then
-          hs.keycodes.setLayout(item.layout)
-        end
-        return
-      end
-    end
-    -- fallback to launch or focus
-    hs.application.launchOrFocus(appName[1])
-  end)
+	if item.callback ~= nil then
+		hs.hotkey.bind(hyper, item.key, item.callback)
+		return
+	end
+	hs.hotkey.bind(hyper, item.key, function()
+		-- if appName is a list, then first look for opened windows of the provided list
+		-- and focus the first we find. If not, then fall back to activate the first app in the list
+		local appName = item.appName -- shortcut and also re-used below for the table fallback
+		if type(item.appName) ~= "table" then
+			appName = { item.appName }
+		end
+		for _, name in ipairs(appName) do
+			-- We prefer to use an already existing window in the current space if possible
+			local appWindows = hs.window.filter.new(false):setAppFilter(name, { currentSpace = true }):getWindows()
+			if #appWindows > 0 then
+				print("found app window in current space", name)
+				appWindows[1]:focus()
+				return
+			end
+			local app = hs.appfinder.appFromName(name)
+			if app then
+				print("found app open", name)
+				app:activate()
+				app:unhide()
+				if item.layout ~= nil then
+					hs.keycodes.setLayout(item.layout)
+				end
+				return
+			end
+		end
+		-- fallback to launch or focus
+		hs.application.launchOrFocus(appName[1])
+	end)
 end)
