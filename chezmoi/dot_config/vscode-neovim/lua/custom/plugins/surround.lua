48799192af636fb2fa0f08fd0ad5bd10ab4269a7
Danielo Rodriguez <rdanielo@gmail.com>
2024-02-01 19:30:54 +0100

    vscode-vim: use proper surround config

--- chezmoi/dot_config/vscode-neovim/lua/custom/plugins/surround.lua
+++ chezmoi/dot_config/vscode-neovim/lua/custom/plugins/surround.lua
@@ -1,10 +1,13 @@
 return {
-  'kylechui/nvim-surround',
-  version = '*', -- Use for stability; omit to use `main` branch for the latest features
-  event = 'VeryLazy',
-  config = function()
-    require('nvim-surround').setup {
-      -- Configuration here, or leave empty to use defaults
-    }
-  end,
+	"wellle/targets.vim", -- Adds more targets to vim's built-in text object motions
+	{
+		"kylechui/nvim-surround", -- The best surround I tried so far
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 }
