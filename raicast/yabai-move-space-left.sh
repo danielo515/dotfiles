341fd1d4092559f7fa3a89d0285f575715f9da26
Danielo Rodriguez <rdanielo@gmail.com>
2024-02-06 08:21:40 +0100

    yabai: raycast move spaces

---
+++ raicast/yabai-move-space-left.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title yabai move space to left
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon ➡️
+# @raycast.packageName danielo
+
+# Documentation:
+# @raycast.description move space to left
+# @raycast.author Daniel Rodríguez Rivero
+# @raycast.authorURL https://github.com/danielo515
+
+# Moves the current space to the left
+yabai -m space --move prev
---
+++ raicast/yabai-move-space-right.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title yabai move space to right
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon ➡️
+# @raycast.packageName danielo
+
+# Documentation:
+# @raycast.description move space to right
+# @raycast.author Daniel Rodríguez Rivero
+# @raycast.authorURL https://github.com/danielo515
+
+# Moves the current space to the right
+yabai -m space --move next
