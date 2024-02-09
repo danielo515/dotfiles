ff4e35c6e23f241166398bc543fe24f76afc920d
Danielo Rodriguez <rdanielo@gmail.com>
2024-02-09 14:13:19 +0100

    remove powerlevel10k prompt

--- zsh/00_zshrc.zsh
+++ zsh/00_zshrc.zsh
@@ -2,13 +2,6 @@
 # export XDG_CONFIG_HOME="$HOME/.config"
 # export DOTFILES="$HOME/.dotfiles"
 
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
 # -- Do not update oh-my-zsh, chezmoi will take care --
 DISABLE_AUTO_UPDATE="true"
 # -- Import HELPER scripts -------------------------------------------------
@@ -28,7 +21,7 @@ ZSH="$HOME/.oh-my-zsh"
 
 # -- Theme ---------------------------------------------------------------------
 export NVIM_TUI_ENABLE_TRUE_COLOR=1
-ZSH_THEME="powerlevel10k/powerlevel10k"
+# ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # -- ZSH Plugins ---------------------------------------------------------------
 plugins=(
@@ -202,8 +195,3 @@ fi
 if [[ -f "$HOME/.aliases" ]]; then
     source $HOME/.aliases
 fi
-
-# - Powerlevel10k load
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-
