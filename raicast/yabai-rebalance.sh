8fa64afd022ad08f9caccf8535fbd8e50d6f45c1
Danielo Rodriguez <rdanielo@gmail.com>
2024-04-12 14:12:15 +0200

    raicast: yabai rebalance

---
+++ raicast/yabai-rebalance.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title yabai rebalance
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName danielo
+
+# Documentation:
+# @raycast.description Rebalance yabay windows
+# @raycast.author danielo
+# @raycast.authorURL https://raycast.com/danielo.es
+
+yabai -m space --balance
+
