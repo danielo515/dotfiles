20176b459cba78df393c232d02156dadd09d84f1
Danielo Rodriguez <rdanielo@gmail.com>
2024-06-23 08:08:10 +0200

    yabai uses meh instead

--- chezmoi/dot_hammerspoon/keyboard/yabai.lua
+++ chezmoi/dot_hammerspoon/keyboard/yabai.lua
@@ -65,8 +65,8 @@ end
 ---@param key string
 ---@param commands string[]
 ---@param fallback string?
-local function alt(key, commands, fallback)
-	hs.hotkey.bind({ "alt" }, key, function()
+local function bind(key, commands, fallback)
+	hs.hotkey.bind({ "alt", "control", "shift" }, key, function()
 		yabai(commands, fallback)
 	end)
 end
@@ -78,7 +78,7 @@ local hyperKey = { "cmd", "shift", "alt", "ctrl" }
 ---@param fallback string?
 local function hyper(key, commands, fallback)
 	if not hs.hotkey.assignable(hyperKey, key) then
-		hs.alert.show("Conflicting hyeper key " .. key)
+		hs.alert.show("Conflicting hyper key " .. key)
 		return
 	end
 	hs.hotkey.bind(hyperKey, key, function()
@@ -92,14 +92,14 @@ hyper("z", { "window --toggle zoom-parent" })
 hyper("g", { "window --toggle float", "window --grid 2:2:1:1:1:1" })
 hyper("right", { "window --east --resize right:50:0" }, "window --resize right:50:0")
 hyper("left", { "window --west --resize right:-50:0" }, "window --resize right:-50:0")
-alt("m", { "space --toggle mission-control" })
+bind("m", { "space --toggle mission-control" })
 -- alt("r", { "space --rotate 90" })
-alt("t", { "window --toggle float", "window --grid 4:4:1:1:2:2" })
+bind("t", { "window --toggle float", "window --grid 4:4:1:1:2:2" })
 
 -- special characters
-alt("'", { "space --layout stack" })
-alt(";", { "space --layout bsp" })
-alt("tab", { "space --focus recent" })
+bind("'", { "space --layout stack" })
+bind(";", { "space --layout bsp" })
+bind("tab", { "space --focus recent" })
 
 ---@param key string
 ---@param commands string[]
@@ -116,15 +116,15 @@ end
 
 for i = 1, 9 do
 	local num = tostring(i)
-	alt(num, { "space --focus " .. num })
+	bind(num, { "space --focus " .. num })
 	altShiftNumber(num)
 end
 
 -- This allows to set a different alternative command for the l and h, which will be easier than handling that in the loop
-alt("l", { "window --focus east" }, "window --focus first")
-alt("h", { "window --focus west" }, "window --focus last")
-alt("j", { "window --focus south" }, "window --focus north")
-alt("k", { "window --focus north" }, "window --focus south")
+bind("l", { "window --focus east" }, "window --focus first")
+bind("h", { "window --focus west" }, "window --focus last")
+bind("j", { "window --focus south" }, "window --focus north")
+bind("k", { "window --focus north" }, "window --focus south")
 altShift("l", { "window --swap east" }, "window --swap first")
 altShift("h", { "window --swap west" }, "window --swap last")
 altShift("j", { "window --swap south" }, "window --swap north")
