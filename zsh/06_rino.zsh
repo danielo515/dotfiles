ee4962e61e7ca5fdcddfbf945010febd7b72eb8f
Danielo Rodriguez <rdanielo@gmail.com>
2025-02-14 10:34:45 +0100

    rhino hook

---
+++ zsh/06_rino.zsh
@@ -0,0 +1,4 @@
+for profile in ${(z)NIX_PROFILES}; do
+  fpath+=($profile/share/zsh/site-functions $profile/share/zsh/$ZSH_VERSION/functions $profile/share/zsh/vendor-completions)
+done
+autoload -U compinit && compinit
